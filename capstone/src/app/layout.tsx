c24a4fc5ccd2be84c7cbda6a92e65e76e6416f9d
Andranique Boone <andranique.boone@gmail.com>
2026-05-13 16:25:13 -0400

    fix children type in layout

--- capstone/src/app/layout.tsx
+++ capstone/src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
+export default function RootLayout({ children }: { children: React.ReactNode }) {    <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
